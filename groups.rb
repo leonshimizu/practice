a72fbedd15122eef2e3546d861897cdddeb663a3
Leon <lmshimizu@gmail.com>
2021-10-05 10:57:49 -0700

    starting next problem - groups.rb

---
+++ groups.rb
@@ -0,0 +1,4 @@
+# Create a program that puts your class into groups! Have the program request the user to enter each student’s name. Assume the classroom has an even number of students, so there are only groups of two.
+# Refactor the program to take an odd or even number of students. If odd, one group should have three students.
+
+p "students"
