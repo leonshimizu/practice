b163cc50d7716fb3b8a3bc46f176f86d050d257b
Leon <lmshimizu@gmail.com>
2021-10-05 10:54:49 -0700

    completed program

--- hashesPractice.rb
+++ hashesPractice.rb
@@ -2,30 +2,44 @@
 # Each person should automatically be given an account number which is a randomized ten digit number.
 # After the user is finished, the program should print out all the users. For example:
 
-users = [
-  {
-    "first_name" => "",
-    "last_name" => "",
-    "email" => ""
-  },
-  {
-    "first_name" => "",
-    "last_name" => "",
-    "email" => ""
-  },
-  {
-    "first_name" => "",
-    "last_name" => "",
-    "email" => ""
-  },
-  {
-    "first_name" => "",
-    "last_name" => "",
-    "email" => ""
-  },
-  {
-    "first_name" => "",
-    "last_name" => "",
-    "email" => ""
-  }
-]
+# users = [
+#   {
+#     "first_name" => "",
+#     "last_name" => "",
+#     "email" => ""
+#   },
+#   {
+#     "first_name" => "",
+#     "last_name" => "",
+#     "email" => ""
+#   },
+#   {
+#     "first_name" => "",
+#     "last_name" => "",
+#     "email" => ""
+#   },
+#   {
+#     "first_name" => "",
+#     "last_name" => "",
+#     "email" => ""
+#   },
+#   {
+#     "first_name" => "",
+#     "last_name" => "",
+#     "email" => ""
+#   }
+# ]
+
+users = []
+
+5.times do
+  puts "Please enter your first name, last name, and email:"
+  fn = gets.chomp
+  ln = gets.chomp
+  e = gets.chomp
+  n = rand(10 ** 10)
+
+  users << {"FIRST": fn, "LAST": ln, "EMAIL": e, "ACCT #": n}
+end
+
+p users
