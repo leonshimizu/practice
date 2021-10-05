f9d17a22be892e622af9850d50d78f753a65f075
Leon <lmshimizu@gmail.com>
2021-10-05 11:07:37 -0700

    created array for all students

--- groups.rb
+++ groups.rb
@@ -1,4 +1,18 @@
 # Create a program that puts your class into groups! Have the program request the user to enter each student’s name. Assume the classroom has an even number of students, so there are only groups of two.
 # Refactor the program to take an odd or even number of students. If odd, one group should have three students.
 
-p "students"
+
+puts "Enter the names of everyone in your class and I'll put them into groups of 2 - Please type 'Done' when finished:"
+
+students = []
+
+while true
+  student = gets.chomp
+  students << student
+  if student.downcase == "done"
+    students.delete_at(-1)
+    break
+  end
+end
+
+p students
