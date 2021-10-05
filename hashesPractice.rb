c71d9116c0fc59984d7feab51890f154dfed2a25
Leon <lmshimizu@gmail.com>
2021-10-05 10:08:31 -0700

    attempting to create new repository

---
+++ hashesPractice.rb
@@ -0,0 +1,4 @@
+# Create a banking program that asks the user 5 times to enter a first name, last name, and email. This information should be stored as an array of hashes.
+# Each person should automatically be given an account number which is a randomized ten digit number.
+# After the user is finished, the program should print out all the users. For example:
+
